6a9f3924c8ae69879be9690038a97219da6f91f6
mrj <marujunyy@qq.com>
2012-12-19 14:19:40 +0800

    first

---
+++ IBooKTurnPagesReader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IBooKTurnPagesReader
+//
+//  Created by  on 11-11-8.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
